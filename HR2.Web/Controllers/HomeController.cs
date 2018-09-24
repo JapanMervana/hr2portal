2ee0dde230caedf3f8f8de111564ea6b81ab3dad
tblocksHR2 <43443560+tblocksHR2@users.noreply.github.com>
2018-09-24 11:48:37 +0530

    OAuth & Home Page

--- HR2.Web/Controllers/HomeController.cs
+++ HR2.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace HR2.Web.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
